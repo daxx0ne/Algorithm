f702555385ce419674bc7ad53b510244b7a0f81f
dawon <jcn07253@gmail.com>
2023-03-04 14:05:53 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String A = in.next();
+
+        System.out.println(A + "??!");
+
+        in.close();
+    }
+}
