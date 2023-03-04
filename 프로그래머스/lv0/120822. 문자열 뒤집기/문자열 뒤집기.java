e57649c89973c3438bbfbc8584c994f0554f7148
dawon <jcn07253@gmail.com>
2023-03-04 19:30:17 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+       
+        StringBuffer sb = new StringBuffer(my_string);
+        String answer = sb.reverse().toString();
+        
+        return answer;
+    }
+}
