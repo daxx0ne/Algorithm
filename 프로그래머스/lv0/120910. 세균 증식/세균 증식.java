2fc6ea6633c97121b6d56dc2ca532b1758996f9e
dawon <jcn07253@gmail.com>
2023-03-10 16:29:35 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int n, int t) {
-        int mul = 0;
-        int answer = 0;
         for (int i = 1; i <= t; i++) {
-            mul = n * 2;
-            n = mul;
-            answer = mul;
+            n *= 2;
         }
-        return answer;
+        return n;
     }
 }
