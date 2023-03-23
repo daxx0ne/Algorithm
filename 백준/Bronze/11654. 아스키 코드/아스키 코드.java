ce2fa323198155a6283d197be5a2e65536659577
Dawon <jcn07253@gmail.com>
2023-03-23 13:35:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.nextLine().charAt(0);
+        System.out.println((int)a);
+    }
+}
