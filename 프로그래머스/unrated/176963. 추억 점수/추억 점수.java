37feec3a4203f20ab22a8f3e212efc56d4221a8c
Dawon <jcn07253@gmail.com>
2023-04-18 20:40:03 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        for(int i = 0; i < photo.length; i++){
+            for(int j = 0; j < photo[i].length; j++){
+                for(int k = 0; k < name.length; k++){
+                    if(name[k].equals(photo[i][j])){
+                        answer[i] += yearning[k];
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
