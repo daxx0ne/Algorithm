20997b2b9451c571d0c690a000730ff8e566c1ae
Dawon <jcn07253@gmail.com>
2023-05-27 21:30:51 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int num1 = 0;
+        int num2 = 0;
+        for(int i = 0; i < num_list.length; i++) {
+            if(i % 2 == 0) {
+                num1 += num_list[i];
+            }
+            else {
+                num2 += num_list[i];
+            }
+        }
+        answer = Math.max(num1, num2);
+        return answer;
+    }
+}
