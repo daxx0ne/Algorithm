7c825ee3ac929a8d348ddc8af595aaa4b79be6b3
Dawon <jcn07253@gmail.com>
2023-06-04 18:22:31 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 10.90 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++){
+            if(i == num1){
+                answer += my_string.charAt(num2);
+            }
+            else if(i == num2){
+                answer += my_string.charAt(num1);
+            }
+            else {
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
