a072ead241e9dd5fad98b3d91ab1b7e4a9591337
Dawon <jcn07253@gmail.com>
2023-06-27 21:46:41 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 196 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		switch (str) {
+			case "SONGDO" -> System.out.println("HIGHSCHOOL");
+			case "CODE" -> System.out.println("MASTER");
+			case "2023" -> System.out.println("0611");
+			case "ALGORITHM" -> System.out.println("CONTEST");
+		}
+	}
+}
