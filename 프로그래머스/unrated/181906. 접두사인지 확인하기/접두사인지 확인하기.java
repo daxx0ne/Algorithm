89507990bad1cfba64dd2d541a7872bfad771d60
Dawon <jcn07253@gmail.com>
2023-07-07 17:43:33 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        if (my_string.startsWith(is_prefix)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
