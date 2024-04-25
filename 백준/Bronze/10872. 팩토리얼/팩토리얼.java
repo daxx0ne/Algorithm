32807f4091020aea3187aee6321037f4e4746876
Dawon <jcn07253@gmail.com>
2024-04-25 13:10:04 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int result = 1;
+        for(int i = n; i > 0; i--) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
