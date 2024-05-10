5e8237248755c60a6502390a68fdeb71f270bf98
Dawon <jcn07253@gmail.com>
2024-05-10 13:00:38 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.72 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String my_string) {
+        my_string = my_string.replaceAll("[a-z]", "");
+        String[] arr = my_string.split("");
+        int answer[] = new int[arr.length];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
